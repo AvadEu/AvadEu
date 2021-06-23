6ba172b9c50f1db04bd08713924697331a9b9133
AvadEu <dutwik44@wp.pl>
2021-06-23 09:48:51 +0200

    Initial js commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function odliczanie()  {
+    
+    var arr = new Array(5);
+
+    for (var i = 0; i<=4; i++) {
+        arr[i] = i+1;
+    } 
+    for (var j = 0; j<=4; j++) {
+        console.log(arr[j]);
+    }
+
+}   
+odliczanie();
