d1536c71e1d8be62f0beefc735ebf5fdd51a9ce6
AvadEu <dutwik44@wp.pl>
2021-06-23 17:09:40 +0200

    Reprograming default js fucntion

--- scripts.js
+++ scripts.js
@@ -1,13 +1,4 @@
-function odliczanie()  {
-    
-    var arr = new Array(5);
-
-    for (var i = 0; i<=4; i++) {
-        arr[i] = i+1;
-    } 
-    for (var j = 0; j<=4; j++) {
-        console.log(arr[j]);
-    }
-
-}   
-odliczanie();
+const testFunction = () => {
+    console.log("This is only a test function :D");
+}
+testFunction();
