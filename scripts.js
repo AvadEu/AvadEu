df1b92ff7722854f9da8fe460777817ed057dc53
AvadEu <dutwik44@wp.pl>
2021-06-24 13:02:21 +0200

    commit once again

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-const testFunction = () => {
-    console.log("This is only a test function :D");
-}
-testFunction();
+(function(){
+    let beerName = "Lech",
+        beerPrice = 4;
+
+    let beerData = {
+        beerName,
+        beerPrice,
+        getFullPrice() {
+            return "Full cost of one botlle of " + this.beerName + " " + this.beerPrice*1.23;
+        }
+    }
+    console.log(beerData.getFullPrice());
+}());
